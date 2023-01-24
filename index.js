83391f3a240fe34a3c658799a559d92a46f8fb36
Rick <rjenkins1029@gmail.com>
2023-01-23 21:44:15 -0800

    changed main index.js

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-require("dotenv").config();
+require('dotenv').config();
 
-//console.log(process.env.JWT_SECRET);
-
-
-const express = require("express");
-const morgan = require("morgan");
-const apiRouter = require("./api");
+const { PORT = 3000 } = process.env;
+const express = require('express');
 const server = express();
-const { client } = require("./db");
-client.connect();
-const PORT = 3000;
+const apiRouter = require('./api');
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
 
-server.use(express.json());
-server.use(morgan("dev"));
-server.use("/api", apiRouter);
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+
+
 server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
-
+    console.log('___Body Logger START___');
+    console.log(req.body);
+    console.log("___Body Logger End___");
+    next();
+});
+server.use('/api', (req, res, next) => {
+  console.log("A request was made to /api");
   next();
 });
 
-server.listen(PORT, () => {
-  console.log("The server is up on port", PORT);
+server.get('/api', (req, res, next) => {
+  console.log("A get request was made to /api");
+  res.send({ message: "success" });
 });
 
-// server.get('/background/:color', (req, res, next) => {
-//   res.send(`
-//     <body style="background: ${ req.params.color };">
-//       <h1>Hello World</h1>
-//     </body>
-//   `);
-// });
+server.use('/api', apiRouter);
 
-// server.get('/add/:first/to/:second', (req, res, next) => {
-//   res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-//     Number(req.params.first) + Number(req.params.second)
-//    }</h1>`);
-// });
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
